97dfa3f2490a345658a5476ac6da3ecc2e56b073
danilodavid521 <daniltos151@gmail.com>
2025-01-07 15:20:34 +0600

    Login and auth with supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js'
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -7,7 +9,9 @@ declare global {
 			session: Session | null;
 			user: User | null;
 		}
-		// interface PageData {}
+		interface PageData {
+			session: Session | null
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import Header from '$lib/components/Header.svelte';
 	import '$lib/styles/app.css';
-	let { children } = $props();
+
+	let { data, children } = $props();
+	let { session, supabase, user } = $derived(data);
+
+	onMount(() => {
+		console.log(user);
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
-<Header />
+<Header {session} />
 <main class="flex h-full min-h-[70vh]">
 	{@render children()}
 </main>
