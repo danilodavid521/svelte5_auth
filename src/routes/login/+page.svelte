98409e13e4091f02b52f95f9e6cf967854ad0632
danilodavid521 <daniltos151@gmail.com>
2025-01-07 22:43:47 +0600

    Login ui update

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,12 +6,8 @@
 	<title>Login</title>
 </svelte:head>
 
-<div class="flex w-full flex-col items-center justify-center bg-background">
-	<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-		<div class="flex flex-col space-y-2 text-center">
-			<h1 class="text-2xl font-semibold tracking-tight">Login</h1>
-			<p class="text-sm text-muted-foreground">Enter your email to login</p>
-		</div>
+<div class="bg-background flex w-full flex-col items-center justify-center">
+	<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:max-w-lg">
 		<UserAuthForm />
 	</div>
 </div>
