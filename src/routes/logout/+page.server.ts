283c8f8af11b5b1d30e3976d1dd1583d8aec7cd7
danilodavid521 <daniltos151@gmail.com>
2025-01-09 11:38:04 +0600

    Fix logout issue

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,5 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 
+export const actions = {
+	logout: async ({ locals }) => {
+		await locals.supabase.auth.signOut();
+		throw redirect(302, '/');
+	}
+};
+
 export const load = async () => {
 	redirect(303, '/login');
 };
